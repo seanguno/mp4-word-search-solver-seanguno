5fff788d6314dc9157adb1fc42c6d78af3538e8b
Sean Guno <91289730+seanguno@users.noreply.github.com>
2023-08-19 22:49:54 -0500

    Update WordSearchSolver.hpp

--- includes/WordSearchSolver.hpp
+++ includes/WordSearchSolver.hpp
@@ -20,6 +20,18 @@ private:
   std::vector<std::vector<char>> puzzle_;
   size_t puzzle_height_;
   size_t puzzle_width_;
+  bool HorizHelper(const std::string& word, size_t starting_row, size_t starting_col);
+  WordLocation HorizWord(size_t starting_row, const std::string& word, size_t starting_col);
+  WordLocation FindHorizWord(const std::string& word);
+  bool VertHelper(const std::string& word, size_t starting_row, size_t starting_col);
+  WordLocation VertWord(size_t starting_row, const std::string& word, size_t starting_col);
+  WordLocation FindVertWord(const std::string& word);
+  bool RightDiagHelper(const std::string& word, size_t starting_row, size_t starting_col);
+  WordLocation RightDiagWord(const std::string& word, size_t starting_row, size_t starting_col); 
+  WordLocation FindRightDiagWord(const std::string& word);
+  bool LeftDiagHelper(const std::string& word, size_t starting_row, size_t starting_col);
+  WordLocation LeftDiagWord(const std::string& word, size_t starting_row, size_t starting_col); 
+  WordLocation FindLeftDiagWord(const std::string& word);
 };
 
 #endif
