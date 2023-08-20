97b0e3e2a4a77c4bbf58449c5729b1c3cc9dd088
Sean Guno <91289730+seanguno@users.noreply.github.com>
2023-08-19 22:48:34 -0500

    Update driver.cc

--- src/driver.cc
+++ src/driver.cc
@@ -9,8 +9,37 @@ int main() {
                                         {'h', 'e', 'l', 'l', 'o'},
                                         {'c', 'a', 'l', 'o', 'm'},
                                         {'a', 'e', 't', 'a', 'u'},
-                                        {'t', 'e', 't', 'o', 'u'}};
-  // WordSearchSolver wss(puzzle);
-  // std::cout << wss.FindWord("hello", CheckDirection::kHorizontal);
-  // std::cout << wss.FindWord("hello");
+                                        {'t', 'e', 't', 'o', 'u'},
+                                        {'a', 'b', 'c', 'd', 'e'}};
+  WordSearchSolver wss(puzzle);
+  std::cout << wss.FindWord("mat", CheckDirection::kLeftDiag);
+  std::cout << wss.FindWord("mat");
+  std::cout << wss.FindWord("hello", CheckDirection::kHorizontal);
+  std::cout << wss.FindWord("hello");
+  std::cout << wss.FindWord("lle", CheckDirection::kLeftDiag);
+  std::cout << wss.FindWord("lle");
+  std::cout << wss.FindWord("hat", CheckDirection::kRightDiag);
+  std::cout << wss.FindWord("hat");
+  std::cout << wss.FindWord("cat", CheckDirection::kVertical);
+  std::cout << wss.FindWord("cat");
+  std::cout << wss.FindWord("muu", CheckDirection::kVertical);
+  std::cout << wss.FindWord("muu");
+  std::cout << wss.FindWord("del", CheckDirection::kRightDiag);
+  std::cout << wss.FindWord("del");
+  std::cout << wss.FindWord("lau", CheckDirection::kRightDiag);
+  std::cout << wss.FindWord("lau");
+  std::cout << wss.FindWord("delau", CheckDirection::kRightDiag);
+  std::cout << wss.FindWord("delau");
+  std::cout << wss.FindWord("au", CheckDirection::kRightDiag);
+  std::cout << wss.FindWord("au");
+  std::cout << wss.FindWord("u", CheckDirection::kRightDiag);
+  std::cout << wss.FindWord("u");
+  std::cout << wss.FindWord("ae", CheckDirection::kRightDiag);
+  std::cout << wss.FindWord("ae");
+  std::cout << wss.FindWord("oo", CheckDirection::kRightDiag);
+  std::cout << wss.FindWord("oo");
+  std::cout << wss.FindWord("toe", CheckDirection::kRightDiag);
+  std::cout << wss.FindWord("toe");
+
+
 }
